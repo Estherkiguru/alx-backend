f4a659d579b8c814ffa59c792806a917368cabc7
EstherKiguru <esipraisewambui@gmail.com>
2024-07-30 23:50:28 +0300

    Supported languages set up

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" Module for get_locale function"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """configuration class for configuration settings"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULE_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+"""configure the Flask instance to use the config class"""
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+
+"""Instantiate the Babel object"""
+babel = Babel(app)
+
+@babel.localeselector
+def get_locale():
+    """Determines best match with supported laguages"""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route("/")
+def home_page():
+    """ Route for the home page """
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)

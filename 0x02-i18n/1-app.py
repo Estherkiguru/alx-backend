e4117a90af1b22630c75154b11b0276b33f983c1
EstherKiguru <esipraisewambui@gmail.com>
2024-07-30 23:30:45 +0300

    Babel set up

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+""" Module for instantiating Babel object in app """
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+"""configuration class for configuration settings"""
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULE_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+"""configure the Flask instance to use the config class"""
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+
+"""Instantiate the Babel object"""
+Babel = Babel(app)
+
+
+@app.route("/")
+def welcome_page():
+    """ Route for the welcome page """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)

b44b3a3ba73fe760c25fef3c5d08254ee2010765
EStherKiguru <esipraisewambui@gmail.com>
2024-08-28 19:12:03 +0300

    job creator

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,23 @@
+import { createQueue } from 'kue';
+
+const queue = createQueue();
+const jobData = {
+	phoneNumber: +23456789,
+	message: 'Account verification code',
+};
+
+const job = queue.create('push_notification_code', jobData).save((error) => {
+	if (error) {
+		console.error(`Error encountered while creating the job`);
+	}else {
+		console.log(`Notification job created: ${job.id}`);
+	}
+	});
+
+job.on('complete', () => {
+	console.log('Notification job completed');
+});
+
+job.on('failed', () => {
+	console.log('Notification job failed');
+});
